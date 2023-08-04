583d9f85323cef6d8c37fee8545ec0ac72efe954
Sophorn Thun <thunsophorn012@gmail.com>
2023-08-04 13:45:56 +0200

    revise PORT

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import { join, dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
