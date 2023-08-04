0805a96be87e7e019ea75ef3bd9a72a37ae5a65c
Sophorn Thun <thunsophorn012@gmail.com>
2023-08-04 14:48:04 +0200

    add connection

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-import cors from 'cors'
+import cors from 'cors';
+import mysql from 'mysql2/promise'
 import express from 'express';
 import { router } from './api.routes.js';
 import { fileURLToPath } from 'url';
 import { join, dirname } from 'path';
+import getConnection from './utils/connection.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,6 +15,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/static', express.static(join(__dirname, 'public')))
 app.use('/', router);
+
+const connectionPool = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
 
 app.all('*', (req, res, next) => {
   res.status(404).json({
