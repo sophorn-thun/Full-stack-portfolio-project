85b3635f8b42468f1853c96f0cc48bc787df9c3c
Sophorn Thun <thunsophorn012@gmail.com>
2023-08-04 13:37:22 +0200

    fix port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import { join, dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
